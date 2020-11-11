7c1114a1b0e0a05f080ce319d9e9a8b00dc3d93b
afonsomedeiros <afonso.b.medeiros@gmail.com>
2020-11-11 09:41:37 -0300

    Monitoria 10-11-2020

---
+++ Monitoria 10-11-2020/src/Components/Categorias/Categorias.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class Categorias extends React.Component {
+    render() {
+        return (
+            <div className="col-lg-3 col-md-2 d-sm-inline">
+                <ul className="border">
+                    <li className="mb-1 mt-1 text-left">
+                        <a className="text-white" href="/">Categoria 1</a>
+                    </li>
+                    <li className="mb-1 mt-1 text-left">
+                        <a className="text-white" href="/">Categoria 2</a>
+                    </li>
+                    <li className="mb-1 mt-1 text-left">
+                        <a className="text-white" href="/">Categoria 3</a>
+                    </li>
+                    <li className="mb-1 mt-1 text-left">
+                        <a className="text-white" href="/">Categoria 4</a>
+                    </li>
+                    <li className="mb-1 mt-1 text-left">
+                        <a className="text-white" href="/">Categoria 5</a>
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Categorias;
---
+++ Monitoria 10-11-2020/src/Components/Galeria/Galeria.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Galeria extends React.Component {
+    render() {
+        console.log(this.props);
+        return (
+            <div className="col-lg-9 col-md-10 col-sm-12 col-12">
+                <div className="galeria">
+                    <img className="mb-3 col-lg-3 col-mg-4 col-sm-5 col-6 float-left" src="https://picsum.photos/id/0/150/150" alt="" />
+                    <img className="mb-3 col-lg-3 col-mg-4 col-sm-5 col-6 float-left" src="https://picsum.photos/id/1/150/150" alt="" />
+                    <img className="mb-3 col-lg-3 col-mg-4 col-sm-5 col-6 float-left" src="https://picsum.photos/id/2/150/150" alt="" />
+                    <img className="mb-3 col-lg-3 col-mg-4 col-sm-5 col-6 float-left" src="https://picsum.photos/id/3/150/150" alt="" />
+                    <img className="mb-3 col-lg-3 col-mg-4 col-sm-5 col-6 float-left" src="https://picsum.photos/id/4/150/150" alt="" />
+                    <img className="mb-3 col-lg-3 col-mg-4 col-sm-5 col-6 float-left" src="https://picsum.photos/id/5/150/150" alt="" />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Galeria;
